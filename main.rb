26bd2cd59d9a5fe1b193f29c8de5e3f60cda1a33
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-08-12 21:28:44 +0300

    Each method add

--- main.rb
+++ main.rb
@@ -6,3 +6,12 @@ def bubble_sort(array)
         # We make sorted true by default and in case we have any swap we will make it false
         sorted = true
         iteration += 1
+
+        # With each loop we iterate all the array elements to check if we need to swap any
+    # if we swap at least one element we set the sorted to false
+    array.each_with_index do |element, index|
+        next_element = array[index + 1]
+        # With each full iteration one more last element is already set
+        # When we have n iteration, n elements from the end of the array are already set
+        # We also break if the next element does not exist
+        break if !next_element && iteration > array.length - index - 1
