3b65859fbcd9c01f93285521dd709a64634d08d8
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2020-08-12 21:28:01 +0300

    loop iteration

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
 def bubble_sort(array)
     # Count how many times did we iterate the array
     iteration = 0
+
+    loop do
+        # We make sorted true by default and in case we have any swap we will make it false
+        sorted = true
+        iteration += 1
